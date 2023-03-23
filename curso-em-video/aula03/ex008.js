c3ddd99d053c00ecfa7419fd5050a85fe5af483e
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-03-23 16:23:52 -0300

    condições

---
+++ curso-em-video/aula03/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+
+console.log(`A velocidade do seu carro é ${vel} `)
+
+if (vel > 60 ){
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+} //condição simple (não tem o else)
+
+console.log(`Dirija sempre usando o sinto de segurança!`)
---
+++ curso-em-video/aula03/ex009.js
@@ -0,0 +1,9 @@
+var país = 'Fança'
+
+console.log(`Se você nasceu em ${país}`)
+
+if (país == 'Brasil') {
+    console.log('você é estadunidense')
+} else {
+    console.log('você é estrangeiro')
+}
