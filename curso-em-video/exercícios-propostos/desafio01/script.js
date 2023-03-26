12e9aaea15524e525146f2e19c850f89317b38cd
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-03-26 00:07:58 -0300

    Update script.js

--- curso-em-video/exercícios-propostos/desafio01/script.js
+++ curso-em-video/exercícios-propostos/desafio01/script.js
@@ -5,19 +5,23 @@ function carregar(params) {
 
     var data = new Date()
     var hours = data.getHours()
+    var minutos = data.getMinutes()
 
-    //var imagemNoite = '/logica-de-programacao/curso-em-video/exercícios-propostos/desafio01/night.png'
-
-    msg_area.innerHTML = `Agora são ${hours} horas`
+    msg_area.innerHTML = `Agora são ${hours}:${minutos} horas`
 
     if (hours >= 0 && hours < 12) {
-        img.scr = 'morning.png'
+        document.querySelector('img').src = '/logica-de-programacao/curso-em-video/exercícios-propostos/img/morning.png'
+
+        document.body.style.background = '#FFFF8E'
 
     } else if (hours >= 12 && hours < 18) {
-        img.scr = '/logica-de-programacao/curso-em-video/exercícios-propostos/img/morning.png'
-        document.body.style.background = 'burlywood'
-        
+        document.querySelector('img').src = '/logica-de-programacao/curso-em-video/exercícios-propostos/img/afternoon.png'
+
+        document.body.style.background = '#F17935'
+
     } else {
+       document.querySelector('img').src = '/logica-de-programacao/curso-em-video/exercícios-propostos/img/night.png'
+
         document.body.style.background = '#2c1e47'
         document.body.style.color = 'grey'
     }
