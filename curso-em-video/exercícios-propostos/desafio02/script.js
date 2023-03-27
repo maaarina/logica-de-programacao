3edb9634d0ea7d2668cc2d366af9ac69ea7db457
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-03-27 16:54:34 -0300

    Create script.js

---
+++ curso-em-video/exercícios-propostos/desafio02/script.js
@@ -0,0 +1,78 @@
+function verificar() {
+    var data = new Date()
+    var anoAtual = data.getFullYear()
+    var formAno = document.getElementById('ano')
+    var resultado = document.getElementById('resultado')
+
+    if (formAno.value.length == 0 || Number(formAno.value) > anoAtual) {
+
+        alert('[ERRO] verifique seu ano de nascimento e tente novamente')
+
+    } else {
+
+        var formSex = document.getElementsByName('sexo')
+        var idade = anoAtual - Number(formAno.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (formSex[0].checked) {
+
+            gênero = 'sexo masculino'
+
+            if (idade >= 0 && idade <= 10) {
+
+                //criança
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/homemCrianca.png')
+
+            } else if (idade >= 10 && idade < 21){
+
+                //jovem
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/homemJovem.png')
+
+            } else if (idade >= 21 && idade < 50) {
+
+                //adulto
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/homemJovem.png')
+
+            } else {
+
+                //velho
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/homemVelho.png')
+
+            }
+
+        } else if (formSex[1].checked) {
+
+            gênero = 'sexo feminino'
+            
+            if (idade >= 0 && idade <= 10) {
+                
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/mulherCrianca.png')
+                //criança
+
+            } else if (idade >= 10 && idade < 21){
+
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/mulherJovem.png')
+                //jovem
+
+            } else if (idade >= 21 && idade < 50) {
+
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/mulherJovem.png')
+                //adulto
+
+            } else {
+
+                img.setAttribute('src', '/logica-de-programacao/curso-em-video/exercícios-propostos/img/mulherVelha.png')
+                //velho
+
+            }
+        }
+
+        resultado.style.textAling ='center'
+        resultado.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+
+        resultado.appendChild(img)
+    }
+
+} 
