8eb5c2e83fcd31fce287acd47523db0c0174a3f2
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-04-02 19:53:41 -0300

    Create teste.js

---
+++ curso-em-video/aula04/teste.js
@@ -0,0 +1,24 @@
+var c = 1
+
+while (c <= 5) {
+    console.log( c + ' test')
+    c++; // c = c +1
+}
+
+
+/*
+do {
+    console.log('test')
+    c++
+} while (c <= 5);
+*/
+
+
+/*
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+*/
