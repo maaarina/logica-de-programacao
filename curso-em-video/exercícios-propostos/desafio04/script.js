608d36f35816302920e2274dfac47cc41aeb678d
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-04-07 23:20:46 -0300

    Update script.js

--- curso-em-video/exercícios-propostos/desafio04/script.js
+++ curso-em-video/exercícios-propostos/desafio04/script.js
@@ -3,18 +3,19 @@ function calcular() {
     let resultado = document.getElementById('divRes')    
     
     if (n.value.length == 0) {
-        alert('Digite um número')
+        alert('Digite um número') // se estiver vazio ... alert
     } else {
-        let n = Number(num.value)
+        
+        let n = Number(num.value) //se não, pegar o número e converte 
         let i = 1
-        resultado.innerHTML = ''
+        resultado.innerHTML = '' //string vazia para a área do resultado estar vazia a cada nova tabuada gerada
 
         while (i <= 10) {
-            let item = document.createElement('option')
+            let item = document.createElement('option') //cria um elemento HTML de forma dinâmica que será inserido no HTML porteriormente
             
-            item.text = `${n} x ${i} = ${n * i}`
-            item.value = `resultado${i}`
-            resultado.appendChild(item)
+            item.text = `${n} x ${i} = ${n * i}` //a parte de dentro do elemento criado ('option')
+            //item.value = `resultado${i}`
+            resultado.appendChild(item) //adicionar elemento filho
             i++
         }
     }
