7912688c549870ac98e6ba99c1cd562e54926bcf
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-04-17 21:54:41 -0300

    Create funcao02.js

---
+++ curso-em-video/aula06/funcao02.js
@@ -0,0 +1,5 @@
+function somar(n1=0 , n2=0 ) { // se algum parâmetro não for definido então irá considerar 0
+    return n1 + n2
+}
+
+console.log(somar (2, 5)) //'somar' é a chamada e '2, 5' são os parâmetros 
