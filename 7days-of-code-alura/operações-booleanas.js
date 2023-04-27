cf1f2bc7807649d569cc7b1cde3969cfb12eabd6
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-04-27 11:55:32 -0300

    Update operações-booleanas.js

--- 7days-of-code-alura/operações-booleanas.js
+++ 7days-of-code-alura/operações-booleanas.js
@@ -43,11 +43,17 @@ if (numeroUm == stringUm) {
     console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
 }
 
-if (numeroTrinta === Number(stringTrinta)) {
+if (numeroTrinta === stringTrinta) {
     console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
 } else {
     console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
 }
+
+/*if (numeroTrinta === Number(stringTrinta)) Para que o retorno seja true {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+} */ 
 
 if (numeroDez == stringDez) {
     console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
