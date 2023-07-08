e818655c670ab61a90ec1b86932f749e4b86222a
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-07-07 21:41:19 -0300

    Create day-4-loop.js

---
+++ 7days-of-code-alura/day-4-loop.js
@@ -0,0 +1,25 @@
+/*Você deve criar um programa que comece com um valor específico pré-definido entre 0 a 10 para o número que você vai adivinhar (7, por exemplo).
+
+Em seguida, o programa vai perguntar para você qual o valor que você deseja chutar e, caso você acerte, ele irá te parabenizar. Caso erre, ele vai te dar mais 2 tentativas.
+
+No fim, caso você não acerte nenhuma vez, ele vai imprimir qual era o número inicial.
+
+Depois que o programinha estiver funcionando, tente usar um número randômico em vez de um número pré-definido.*/
+
+const numero = Math.floor(Math.random() * (10 - 0 + 1) + 0);
+let chute = "";
+let acertou = false;
+
+for(let contador = 0; contador < 3; contador++){    
+    chute = prompt("Tente adivinhar o número de 0 a 10:");
+    
+    if(chute == numero){
+        alert(`Parabéns, você acertou! O número era ${numero}.`);
+        acertou = true;
+        break;
+    }
+    alert("Errado!");
+}
+if(!acertou){
+    alert(`Infelizmente, você não acertou. O número era ${numero}!`);
+}
