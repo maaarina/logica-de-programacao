431b3b1e682b254c778d82aeadb03e91648dbd89
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-07-07 21:41:27 -0300

    Create day-6-remove-arrays.js

---
+++ 7days-of-code-alura/day-6-remove-arrays.js
@@ -0,0 +1,13 @@
+/*Você deverá criar a opção de remover algum item da lista, que será exibida junto à pergunta de "você deseja adicionar uma comida na lista de compras"?
+
+A partir daí, caso a pessoa escolha essa opção, o programa irá imprimir os elementos presentes na lista atual, e a pessoa deverá escrever qual deles deseja remover.
+
+Depois disso, o programa irá remover o elemento da lista e imprimir a confirmação de que o item realmente não está mais lá.
+
+Por fim, ele voltará para o ciclo inicial de perguntas.
+
+Se, na hora de deletar o item, o mesmo não for encontrado na lista, você deverá exibir uma mensagem avisando isso.
+
+Por exemplo: "Não foi possível encontrar o item dentro da lista!"
+
+Lembre-se que a opção de remover um item só deverá estar disponível a partir do momento que existir ao menos um elemento dentro da lista de compras. */
